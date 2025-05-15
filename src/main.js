33cd95751e2b8aa924fa50450d963742a73ab206
Nelly <nelly.babillon@gmail.com>
2025-05-15 23:25:40 +0200

    Ajout Varlet UI library + steps entre formulaires

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Varlet from '@varlet/ui'
+import '@varlet/ui/es/style'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(Varlet)
 app.mount('#app')
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SignUpForm from '@/components/signup/SignUpForm.vue'
+</script>
+
+<template>
+  <main>
+    <SignUpForm />
+  </main>
+</template>
