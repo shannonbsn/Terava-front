d89fa72de86b263faf3de8a7378754cf2f0f31b8
Nelly <nelly.babillon@gmail.com>
2025-05-16 09:06:56 +0200

    feat: ajouter le composant MeetTravellers à la vue OnBoarding et supprimer le contenu redondant

--- src/views/OnBoardingView.vue
+++ src/views/OnBoardingView.vue
@@ -4,6 +4,7 @@ import TheFooter from '@/components/TheFooter.vue';
 import BlockCompatibles from '@/components/account/BlockCompatibles.vue';
 import SearchForm from '@/components/onboarding/SearchForm.vue';
 import CurrentStatus from '@/components/onboarding/CurrentStatus.vue';
+import MeetTravellers from '@/components/onboarding/MeetTravellers.vue';
 import { RouterLink } from 'vue-router';
 import { onMounted, onUnmounted, reactive, ref } from 'vue'
 
@@ -34,26 +35,7 @@ const cardsArray = [
     </div>
     <BlockCompatibles headline="Mes voyages prévus" :larger="true" :cards="cardsArray" />
     <RouterLink to="/match" class="navigation--block meetings">
-      <h2>Rencontrer des voyageurs</h2>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-        class="lucide lucide-waypoints-icon lucide-waypoints">
-        <circle cx="12" cy="4.5" r="2.5" />
-        <path d="m10.2 6.3-3.9 3.9" />
-        <circle cx="4.5" cy="12" r="2.5" />
-        <path d="M7 12h10" />
-        <circle cx="19.5" cy="12" r="2.5" />
-        <path d="m13.8 17.7 3.9-3.9" />
-        <circle cx="12" cy="19.5" r="2.5" />
-      </svg>
-      <div class="users--wrapper flex">
-        <div class="thumbnail"><img src="../assets/profil-bertrand.jpg" alt="Photo profil" /></div>
-        <div class="thumbnail"><img src="../assets/profil-katerina.jpg" alt="Photo profil" /></div>
-        <div class="thumbnail"><img src="../assets/profil-joyce.jpg" alt="Photo profil" /></div>
-        <div class="thumbnail"><img src="../assets/profil-jorge.jpg" alt="Photo profil" /></div>
-        <div class="thumbnail"><img src="../assets/profil-layla.jpg" alt="Photo profil" /></div>
-      </div>
-
+      <MeetTravellers/>
     </RouterLink>
     <div class="navigation--block">
       <h2>Organiser un voyage</h2>
@@ -102,24 +84,10 @@ h2{
   border-bottom: 1px solid var(--grey-medium);
 }
 
-.users--wrapper .thumbnail{
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  overflow: hidden;
-  margin-left: -10px;
-}
-
-.users--wrapper .thumbnail img{
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-.meetings{
+.meetings {
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 0;
 }
-
 </style>
