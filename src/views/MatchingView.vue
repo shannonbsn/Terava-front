93b6a35f4423a99d9d22177af01db8a77edd9c50
Nelly <nelly.babillon@gmail.com>
2025-05-16 11:10:26 +0200

    feat: mettre à jour les styles des boutons dans MatchingView

--- src/views/MatchingView.vue
+++ src/views/MatchingView.vue
@@ -50,8 +50,14 @@ const offset = computed(() => circumference * (1 - percent.value / 100))
         <span class="percent">{{ percent }}%</span>
       </div>
       <h1>C'est un match!</h1>
-      <button class="btn outline">Discuter avec Flora</button>
+      <button class="btn blue">Discuter avec Flora</button>
       <button class="btn orange flex">Voir profil <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+          viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+          stroke-linejoin="round" class="lucide lucide-arrow-right-icon lucide-arrow-right">
+          <path d="M5 12h14" />
+          <path d="m12 5 7 7-7 7" />
+        </svg></button>
+      <button class="btn outline flex">Passer<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
           viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
           stroke-linejoin="round" class="lucide lucide-arrow-right-icon lucide-arrow-right">
           <path d="M5 12h14" />
