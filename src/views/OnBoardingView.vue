e2bd0c1ecbcb9a997bbe7932411b69fef3347e1a
Nelly <nelly.babillon@gmail.com>
2025-05-16 06:33:29 +0200

    Refactor code structure for improved readability and maintainability

--- src/views/OnBoardingView.vue
+++ src/views/OnBoardingView.vue
@@ -37,43 +37,76 @@ const readonly = ref(false)
 <template>
   <TheHeader />
   <main class="grid">
-    <div class="info-traveller">
-      <h3>Localisation actuelle</h3>
-      <span>Madagascar</span>
+    <div class="info-traveller flex">
+      <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+          class="lucide lucide-map-pin-icon lucide-map-pin">
+          <path
+            d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+          <circle cx="12" cy="10" r="3" />
+        </svg> Madagascar</span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+        class="lucide lucide-pencil-icon lucide-pencil">
+        <path
+          d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+        <path d="m15 5 4 4" />
+      </svg>
     </div>
     <div class="navigation--block">
       <h2>Chercher un voyage</h2>
       <var-form ref="form" :disabled="disabled" :readonly="readonly" scroll-to-error="start">
         <var-space direction="column" :size="[14, 0]">
-          <var-input placeholder="Please input username" :rules="v => !!v || 'The username cannot be empty'"
-            v-model="formData.username" />
-          <var-input type="password" placeholder="Please input password"
-            :rules="[v => !!v || 'The password cannot be empty', (v) => v.length >= 8 || 'The password cannot be less than 8 characters']"
-            v-model="formData.password" />
-          <var-select placeholder="Please select department" :rules="v => !!v || 'The select cannot be empty'"
-            v-model="formData.department">
-            <var-option label="Eat department" />
-            <var-option label="Sleep department" />
-            <var-option label="Play game department" />
+          <var-input placeholder="Mots-clefs" v-model="formData.keyword" />
+          <var-select placeholder="Choisir un pays" v-model="formData.country">
+            <var-option label="France" />
+            <var-option label="Pologne" />
+            <var-option label="Brésil" />
           </var-select>
+          <var-input placeholder="Chercher une ville" v-model="formData.city" />
         </var-space>
       </var-form>
     </div>
-    <BlockCompatibles headline="Mes voyages" :larger="true" />
+    <BlockCompatibles headline="Mes voyages prévus" :larger="true" />
+    <RouterLink to="/matching" class="navigation--block meetings">
+      <h2>Rencontrer des voyageurs</h2>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+        class="lucide lucide-waypoints-icon lucide-waypoints">
+        <circle cx="12" cy="4.5" r="2.5" />
+        <path d="m10.2 6.3-3.9 3.9" />
+        <circle cx="4.5" cy="12" r="2.5" />
+        <path d="M7 12h10" />
+        <circle cx="19.5" cy="12" r="2.5" />
+        <path d="m13.8 17.7 3.9-3.9" />
+        <circle cx="12" cy="19.5" r="2.5" />
+      </svg>
+      <div class="users--wrapper flex">
+        <div class="thumbnail"><img src="../assets/profil-bertrand.jpg" alt="Photo profil" /></div>
+        <div class="thumbnail"><img src="../assets/profil-katerina.jpg" alt="Photo profil" /></div>
+        <div class="thumbnail"><img src="../assets/profil-joyce.jpg" alt="Photo profil" /></div>
+        <div class="thumbnail"><img src="../assets/profil-jorge.jpg" alt="Photo profil" /></div>
+        <div class="thumbnail"><img src="../assets/profil-layla.jpg" alt="Photo profil" /></div>
+      </div>
 
-    <router-link to="/travel/create" class="navigation--block">
+    </RouterLink>
+    <div class="navigation--block">
       <h2>Organiser un voyage</h2>
-    </router-link>
-    <router-link to="/matching" class="navigation--block">
-      <h2>Rencontrer</h2>
-    </router-link>
+      <RouterLink to="/travel/create">
+        <button class="btn blue large">Créer un voyage</button>
+      </RouterLink>
+      <RouterLink to="/travel/create">
+        <button class="btn blue outline large">Gérer mes voyages</button>
+      </RouterLink>
+    </div>
+
   </main>
   <TheFooter />
 </template>
 
 <style scoped>
 main {
-  grid-template-rows: auto,repeat(3, 1fr);
+  grid-template-rows: auto,repeat(4, 1fr);
   gap: 20px;
 }
 
@@ -81,23 +114,59 @@ a {
   text-decoration: none;
 }
 
+h2{
+  margin:0;
+}
+
 .info-traveller{
-  padding: 20px;
+  padding: 15px;
+  justify-content: space-between;
   border-radius: 10px;
   background: var(--orange);
+  color: #fff;
+}
+
+.info-traveller span{
+  display: flex;
+  align-items: center;
+  gap: 10px;
 }
 
 .navigation--block {
-  /* background-color: var(--blue-lighter); */
-  border:1px solid var(--blue-light);
+  /* border:1px solid var(--blue-light); */
   border-radius: 10px;
   align-self: stretch;
   display: flex;
+  gap:15px;
   flex-direction: column;
   justify-content: flex-start;
-  /* align-items: center; */
-  padding: 20px;
+  padding: 5px 20px;
   box-sizing: border-box;
   width: 100%;
+}
+
+.navigation--block:not(:last-child){
+  padding-bottom: 30px;
+  border-bottom: 1px solid var(--grey-medium);
+}
+
+.users--wrapper .thumbnail{
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  overflow: hidden;
+  margin-left: -10px;
+}
+
+.users--wrapper .thumbnail img{
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.meetings{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
 }
 </style>
